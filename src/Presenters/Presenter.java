96c0a248df1d6bc1296bff998983f649fbcab318
SAMi-Rn <sami_roudgarian@yahoo.com>
2023-11-16 13:20:41 -0800

    Iview has been added to Presenter

--- src/Presenters/Presenter.java
+++ src/Presenters/Presenter.java
@@ -1,13 +1,38 @@
 package Presenters;
 
 import Models.Repository;
+import Validation.InputValidation;
+
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Presenter {
-    public String getFile(Date startDate, Date endDate) {
-        Repository repository = Repository.getInstance();
+    public interface IView
+    {
+        void displayResult(String results);
+    }
+
+    private final IView view;
+    private final Repository repository;
+
+    public Presenter(IView view) {
+        this.view = view;
+        repository = Repository.getInstance();
+    }
+    public void getFile(String input) throws Exception {
+        String[] dates = input.split(",");
+
+        if (!InputValidation.isValidDate(dates)) {
+            return;
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Date startDate = format.parse(dates[0]);
+        Date endDate = format.parse(dates[1]);
+
         ArrayList<File> files = repository.search(startDate, endDate);
         StringBuilder result = new StringBuilder();
         if (files != null) {
@@ -15,6 +40,6 @@ public class Presenter {
                 result.append(f.getPath()).append(" ");
             }
         }
-        return result.toString();
+        view.displayResult(result.toString());
     }
 }
--- src/Views/ActivityMain.java
+++ src/Views/ActivityMain.java
@@ -1,31 +1,16 @@
 package Views;
 
 import Presenters.Presenter;
-import Validation.InputValidation;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class ActivityMain extends Activity implements OnInputListener {
+public class ActivityMain extends Activity implements OnInputListener, Presenter.IView {
+    private Presenter presenter;
     public void onCreate() {
         super.onCreate();
+        presenter = new Presenter(this);
         System.out.println("Input StartDate,EndDate in (YYYYMMDD_HHMMSS) format and hit enter:");
     }
     public void onInput(String input) throws Exception {
-        String[] dates = input.split(",");
-
-        if (!InputValidation.isValidDate(dates)) {
-            return;
-        }
-
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        Date startDate = format.parse(dates[0]);
-        Date endDate = format.parse(dates[1]);
-
-        Presenter presenter = new Presenter();
-        String result = presenter.getFile(startDate, endDate);
-
-        displayResult(result);
+        presenter.getFile(input);
     }
     public void displayResult(String results) {
         System.out.println(results);
